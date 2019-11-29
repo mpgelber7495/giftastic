3451ef94a5837793dddd63cc49f3ad3781c691b5
Michael Gelber <mpgelber7495@gmail.com>
2019-11-29 13:24:26 -0500

    Added ability to turn a still giph to animated

--- assets/javascript/apps.js
+++ assets/javascript/apps.js
@@ -1,16 +1,34 @@
 var searchParam = "matthew McConoughey";
-
+var apiResponse;
 function addGiphsToDOM(searchTerm) {
   var apiURL = `https://api.giphy.com/v1/gifs/search?q=${searchTerm}&limit=10&rating=r&&api_key=2TnlxruJDjOfB4XdGy6PoxtRgiJUgXZM`;
   $.ajax(apiURL).then(function(response) {
-    var apiResponse = response.data;
+    apiResponse = response.data;
     console.log(apiResponse);
     for (var i = 0; i < apiResponse.length; i++) {
       $(".giph-holder").append(
-        `<img src ="${apiResponse[i]["images"]["fixed_height_small"]["url"]}">`
+        `<img data-motion = "still" id = ${i} src ="${apiResponse[i]["images"]["fixed_height_small_still"]["url"]}">`
       );
     }
   });
 }
+
+$(".giph-holder").click(function(event) {
+  var giphToChange = $(event.target);
+  if (giphToChange.attr("data-motion", "still")) {
+    giphToChange.attr(
+      "src",
+      apiResponse[giphToChange[0]["id"]]["images"]["fixed_height_small"]["url"]
+    );
+    giphToChange.attr("data-motion", "animated");
+  } else if (giphToChange.attr("data-motion", "animated")) {
+    giphToChange.attr(
+      "src",
+      apiResponse[giphToChange[0]["id"]]["images"]["fixed_height_small_still"][
+        "url"
+      ]
+    );
+  }
+});
 
 addGiphsToDOM("Skunk");
