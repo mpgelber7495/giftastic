ea65687d92731319ecdd5de9be7001ade061befa
Michael Gelber <mpgelber7495@gmail.com>
2019-11-29 13:36:18 -0500

    Added buttons

--- assets/javascript/apps.js
+++ assets/javascript/apps.js
@@ -1,5 +1,18 @@
 var searchParam = "matthew McConoughey";
 var apiResponse;
+
+function addButtons() {
+  var buttonHolder = $(".button-holder");
+  for (var i = 0; i < actors.length; i++) {
+    var button = $("<button>");
+    button.attr("value", actors[i]);
+    button.addClass("actor-button");
+    button.text(actors[i]);
+    buttonHolder.append(button);
+  }
+}
+addButtons();
+
 function addGiphsToDOM(searchTerm) {
   var apiURL = `https://api.giphy.com/v1/gifs/search?q=${searchTerm}&limit=10&rating=r&&api_key=2TnlxruJDjOfB4XdGy6PoxtRgiJUgXZM`;
   $.ajax(apiURL).then(function(response) {
@@ -7,7 +20,7 @@ function addGiphsToDOM(searchTerm) {
     console.log(apiResponse);
     for (var i = 0; i < apiResponse.length; i++) {
       $(".giph-holder").append(
-        `<img data-motion = "still" id = ${i} src ="${apiResponse[i]["images"]["fixed_height_small_still"]["url"]}">`
+        `<img class = "giph-image" data-motion = "still" id = ${i} src ="${apiResponse[i]["images"]["fixed_height_small_still"]["url"]}">`
       );
     }
   });
@@ -15,20 +28,19 @@ function addGiphsToDOM(searchTerm) {
 
 $(".giph-holder").click(function(event) {
   var giphToChange = $(event.target);
-  if (giphToChange.attr("data-motion", "still")) {
+  if (giphToChange.attr("data-motion") === "still") {
     giphToChange.attr(
       "src",
       apiResponse[giphToChange[0]["id"]]["images"]["fixed_height_small"]["url"]
     );
     giphToChange.attr("data-motion", "animated");
-  } else if (giphToChange.attr("data-motion", "animated")) {
+  } else if (giphToChange.attr("data-motion") === "animated") {
     giphToChange.attr(
       "src",
       apiResponse[giphToChange[0]["id"]]["images"]["fixed_height_small_still"][
         "url"
       ]
     );
+    giphToChange.attr("data-motion", "still");
   }
 });
-
-addGiphsToDOM("Skunk");
