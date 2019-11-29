10f03aa0fef9866aff5f8f324dae7eba86bd803b
Michael Gelber <mpgelber7495@gmail.com>
2019-11-29 13:55:42 -0500

    Can add a new button and select it to call giphy

--- assets/javascript/apps.js
+++ assets/javascript/apps.js
@@ -3,6 +3,7 @@ var apiResponse;
 
 function addButtons() {
   var buttonHolder = $(".button-holder");
+  buttonHolder.html("");
   for (var i = 0; i < actors.length; i++) {
     var button = $("<button>");
     button.attr("value", actors[i]);
@@ -10,15 +11,19 @@ function addButtons() {
     button.text(actors[i]);
     buttonHolder.append(button);
   }
+  listenForClicks();
 }
 addButtons();
 
-$(".actor-button").click(function(event) {
-  addGiphsToDOM($(this).val());
-});
+function listenForClicks() {
+  $(".actor-button").click(function(event) {
+    addGiphsToDOM($(this).val());
+  });
+}
 
 function addGiphsToDOM(searchTerm) {
   var apiURL = `https://api.giphy.com/v1/gifs/search?q=${searchTerm}&limit=10&rating=r&&api_key=2TnlxruJDjOfB4XdGy6PoxtRgiJUgXZM`;
+  $(".giph-holder").html("");
   $.ajax(apiURL).then(function(response) {
     apiResponse = response.data;
     console.log(apiResponse);
@@ -47,4 +52,12 @@ $(".giph-holder").click(function(event) {
     );
     giphToChange.attr("data-motion", "still");
   }
+});
+
+// Code to add a new button
+$("#input-button").click(function(event) {
+  event.preventDefault();
+  var newActor = $("#input-field").val();
+  actors.push(newActor);
+  addButtons();
 });
